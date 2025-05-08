58424ff993b259bbaf7b98cd82817e752913b073
Roberts Slisans <rsxdalv@gmail.com>
2025-05-08 19:02:30 +0300

    add CPU offloading

--- extension_ace_step/gradio_app.py
+++ extension_ace_step/gradio_app.py
@@ -9,19 +9,33 @@ REPO_ID = "ACE-Step/ACE-Step-v1-3.5B"
 CHECKPOINT_DIR = "data/models/ace_step/"
 # CHECKPOINT_DIR = "data/models/ace_step/default/"
 USE_HALF_PRECISION = False
+USE_TORCH_COMPILE = False
+USE_CPU_OFFLOAD = False
 
 
 @manage_model_state("ace_step")
 def get_model(
     model_name=REPO_ID,
     use_half_precision=None,
-    torch_compile=False,
+    torch_compile=None,
     checkpoint_dir=CHECKPOINT_DIR,
+    cpu_offload=None,
 ):
     from acestep.pipeline_ace_step import ACEStepPipeline
 
     if use_half_precision is None:
         use_half_precision = USE_HALF_PRECISION
+    
+    if torch_compile is None:
+        torch_compile = USE_TORCH_COMPILE
+
+    if cpu_offload is None:
+        cpu_offload = USE_CPU_OFFLOAD
+
+    # if use_half_precision:
+    #     cpu_offload = True
+    # else:
+    #     cpu_offload = False
 
     dtype = "bfloat16" if use_half_precision else "float32"
 
@@ -29,15 +43,17 @@ def get_model(
         checkpoint_dir=checkpoint_dir,
         dtype=dtype,
         torch_compile=torch_compile,
+        cpu_offload=cpu_offload,
     )
 
     return model_demo
 
 
-def switch_half_precision(use_half_precision, use_torch_compile):
-    global USE_HALF_PRECISION, USE_TORCH_COMPILE
+def switch_half_precision(use_half_precision, use_torch_compile, use_cpu_offload):
+    global USE_HALF_PRECISION, USE_TORCH_COMPILE, USE_CPU_OFFLOAD
     USE_HALF_PRECISION = use_half_precision
     USE_TORCH_COMPILE = use_torch_compile
+    USE_CPU_OFFLOAD = use_cpu_offload
     unload_model("ace_step")
     return "Model will be reloaded when you run the next generation."
 
@@ -73,6 +89,8 @@ def ui():
         Note: The extension does not currently support automatic file saving.
 
         Weights size: 8gb. VRAM ~22gb at full, 11gb at half precision
+
+        CPU Offload can be enabled to reduce VRAM usage, but it does slow down the generation process, especially for short generations.
     """
     )
 
@@ -86,13 +104,14 @@ def ui():
                 label="Use torch compile",
                 value=False,
             )
+            use_cpu_offload = gr.Checkbox(
+                label="Use CPU offload",
+                value=False,
+            )
             reload_model_button = gr.Button("Apply Settings and Unload Model")
             reload_model_button.click(
-                fn=lambda use_half_precision, use_torch_compile: switch_half_precision(
-                    use_half_precision,
-                    use_torch_compile,
-                ),
-                inputs=[use_half_precision, use_torch_compile],
+                fn=switch_half_precision,
+                inputs=[use_half_precision, use_torch_compile, use_cpu_offload],
                 outputs=[reload_model_button],
                 api_name="ace_step_reload_model",
             )
